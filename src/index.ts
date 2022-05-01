ab4e889d20721a3862c4ba714a2081925d197ae5
weidhuang <weidhuang@qq.com>
2022-05-01 22:31:06 +0800

    remove useless load file

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ const setUpApp = async () => {
   const myapp = DarukServer();
 
   myapp.loadFile('controllers');
-  // myapp.loadFile('services');
-  // myapp.loadFile('repositories');
 
   await myapp.binding();
   myapp.listen(3000);
